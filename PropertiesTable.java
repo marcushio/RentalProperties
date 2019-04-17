c7bbe6fddf84f309850e99a5fece49c22de37343
Marcus Trujillo <marcus.t.trujillo@gmail.com>
2019-04-17 16:10:05 -0600

    We now have tabbed pane functionality

---
+++ PropertiesTable.java
@@ -0,0 +1,10 @@
+/**
+ * Class Description
+ *
+ * @author Marcus Trujillo
+ * @version CS2251 - Intermediate Programming
+ * email: mtrujillo255@cnm.edu
+ * assignment due date:
+ */
+public class PropertiesTable {
+}

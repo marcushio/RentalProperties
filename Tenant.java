563984ae795f81b1857ed8c412bc5893d4e432dc
Marcus Trujillo <marcus.t.trujillo@gmail.com>
2019-04-18 21:40:48 -0600

    added gitignore

---
+++ Tenant.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+/**
+ * Class Description
+ *
+ * @author Marcus Trujillo
+ * @version CS2251 - Intermediate Programming
+ * email: mtrujillo255@cnm.edu
+ * assignment due date:
+ */
+public class Tenant {
+}
